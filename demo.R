83165d7d3a3a45086d260cedc9c3d83821b7b0dc
abaya-m <meaganmonetteabaya@gmail.com>
2025-09-05 15:25:58 -0500

    add mpg plot

--- demo.R
+++ demo.R
@@ -1,1 +1,10 @@
 rm(list = ls())
+library(ggplot2)
+install.packages("ggplot2")
+install.packages("dplyr")
+y
+library(ggplot2)
+library(dplyr)
+avg_mpg <- mpg %>%
+  group_by(class) %>%
+  summarise(avg_hwy_mpg = mean(hwy, na.rm = TRUE))
